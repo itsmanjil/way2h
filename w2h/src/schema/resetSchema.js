44cff1b59546e36cf4792cb9e2a0bcbd159e8b6f
Manjil shrestha <itsmanjil@gmail.com>
2022-12-13 11:27:13 +0545

    reset

--- w2h/src/schema/resetSchema.js
+++
@@ -1,5 +0,0 @@
-import * as yup from "yup";
-
-export const ResetSchema = yup.object().shape({
-  email: yup.string().required("Email Required"),
-});
--- w2h/src/schema/signUpSchema.js
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-
-export const schemaSignUp = yup.object().shape({
-    name:yup.string().required("Name Require"),
-    email: yup.string().required("Email Required"),
-    password: yup.string().required("Password Required"),
-  })
